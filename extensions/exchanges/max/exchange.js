32c6265dd81880b87ed53e46094f38c6bb9a027e
TaopaiC <pctao.blog@pctao.org>
2019-07-01 16:07:33 +0800

    eslint

--- extensions/exchanges/max/exchange.js
+++ extensions/exchanges/max/exchange.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 const ccxt = require('ccxt')
 const path = require('path')
 
@@ -10,6 +9,7 @@ module.exports = function container (conf) {
     return public_client
   }
 
+  // eslint-disable-next-line no-unused-vars
   function authedClient() {
     if (!authed_client) {
       if (!conf.max || !conf.max.key || !conf.max.key === 'YOUR-API-KEY') {
@@ -20,6 +20,7 @@ module.exports = function container (conf) {
     return authed_client
   }
 
+  // eslint-disable-next-line no-unused-vars
   function joinProduct(product_id) {
     return product_id.split('-')[0] + '/' + product_id.split('-')[1]
   }
@@ -43,29 +44,38 @@ module.exports = function container (conf) {
       return require('./products.json')
     },
 
+    // eslint-disable-next-line no-unused-vars
     getTrades: function (opts, cb) {
       throw new Error('getTrades not implemented')
     },
 
+    // eslint-disable-next-line no-unused-vars
     getBalance: function (opts, cb) {
       throw new Error('getBalance not implemented')
     },
 
+    // eslint-disable-next-line no-unused-vars
     getQuote: function (opts, cb) {
       throw new Error('getQuote not implemented')
     },
+
+    // eslint-disable-next-line no-unused-vars
     cancelOrder: function (opts, cb) {
       throw new Error('cancelOrder not implemented')
     },
+    // eslint-disable-next-line no-unused-vars
     buy: function (opts, cb) {
       throw new Error('buy not implemented')
     },
+    // eslint-disable-next-line no-unused-vars
     sell: function (opts, cb) {
       throw new Error('sell not implemented')
     },
+    // eslint-disable-next-line no-unused-vars
     getOrder: function (opts, cb) {
       throw new Error('getOrder not implemented')
     },
+    // eslint-disable-next-line no-unused-vars
     getCursor: function (trade) {
       throw new Error('getCursor not implemented')
     },
